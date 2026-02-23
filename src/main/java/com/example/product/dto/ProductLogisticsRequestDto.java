355a95970c7fce51702c419b4f3384792d7fb36b
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2026-02-23 19:48:14 +0530

    Add validation annotations to ProductLogisticsRequestDto

--- src/main/java/com/example/product/dto/ProductLogisticsRequestDto.java
+++ src/main/java/com/example/product/dto/ProductLogisticsRequestDto.java
@@ -1,5 +1,7 @@
 package com.example.product.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductLogisticsRequestDto {
+    
+    @DecimalMin(value = "0.0", inclusive = false, message = "Shipping weight must be greater than 0")
     private Double shippingWeightKg;
+    
     private String volumeCubicCm;
+    
+    @Size(max = 100, message = "Country of origin cannot exceed 100 characters")
     private String countryOfOrigin;
+    
     private String customsCode;
 }
